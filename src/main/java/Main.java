e465b710d5530f453eb2727c976960662d4ffab8
millergallegof <miller.gallegof@gmail.com>
2022-07-08 00:38:35 -0500

    Se Realiza Ejercicio Kata 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,16 +4,16 @@ import static org.junit.Assert.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Kata1.execute());
+//        System.out.println(Kata1.execute());
         assertEquals(Kata1.execute().size(), 4);
         System.out.println("Kata 1 solved!");
-//
-//        assertEquals(Kata2.execute().size(), 2);
-//        System.out.println("Kata 2 solved!");
-//
-//        assertEquals(Kata3.execute().size(), 4);
-//        System.out.println("Kata 3 solved!");
-//
+
+        assertEquals(Kata2.execute().size(), 2);
+        Kata2.execute();
+
+        assertEquals(Kata3.execute().size(), 4);
+        System.out.println("Kata 3 solved!");
+
 //        assertEquals(Kata4.execute().size(), 4);
 //        System.out.println("Kata 4 solved!");
 //
